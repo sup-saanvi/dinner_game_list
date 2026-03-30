0e3e63e9603f5066c9f0dcc6d48770382bb49456
Abby is good! <saanvip904@gmail.com>
2026-03-30 14:48:18 -0700

    I created pseudo
    I created pseudo code so I know what my code is going to be about and how to structure it right.

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+This is a game where I make a guest list and see how
+many people are coming.
+
+"""
+# create guest 
+def create_guest():
+   '''add guest name to the list.'''
+    # get guest input 
+    def add_guest():
+        name = input("enter guest name: ").strip().title()
+        
+
+# change the guest
+
+# kick out guest 
+
+# sort guests by height
+
+# show how many guest broke in to the party
+
+# show the invitations 
+
+# check for imposters 
+
+# chuck the imposters out 
+
+
